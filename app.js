83a478c86a8cbcb0541dc81c9f4d9c8a0eb4d7ac
Piotrek <amrozp@gmail.com>
2020-09-22 00:11:13 +0200

    init

---
+++ app.js
@@ -0,0 +1,13 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+app.use(express.static(__dirname + '/public'));
+
+// viewed at http://localhost:8080
+app.get('/', function(req, res) {
+    // let path = path.join(__dirname + '/index.html')
+    // console.log(__dirnam)
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(8080);
